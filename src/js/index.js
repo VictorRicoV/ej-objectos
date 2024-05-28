a1ef347e94f391abf48ee3939b51d4bbd83e6eb6
Nathalia <lessricc@gmail.com>
2024-05-28 21:10:57 +0200

    finalizado

--- src/js/index.js
+++ src/js/index.js
@@ -10,19 +10,19 @@ const user = {
   hobbies: ['leer', 'tocar la guitarra', 'pasear con las cabras'],
   pets: [
     { name: 'Max', type: 'perro' },
-    { name: 'Whiskers', type: 'gato' }
+    { name: 'Whiskers', type: 'gato' },
   ],
   address: {
     street: '123 Main Street',
     city: 'Gotham',
     state: 'California',
     postalCode: '12345',
-    country: 'USA'
+    country: 'USA',
   },
   phone: '+1234567890',
   email: 'johndoe@example.com',
   occupation: 'Ingeniero de software',
-  education: 'Master en ciencia de datos'
+  education: 'Master en ciencia de datos',
 };
 
 console.log(
@@ -35,19 +35,19 @@ const data = {
   numbers: [10, 32, 31, 67, 9, 2, 51, 4],
   firstFloor: {
     secondFloor: {
-      numbersPlus2: []
+      numbersPlus2: [],
     },
     thirdFloor: {
-      numbersDouble: []
-    }
+      numbersDouble: [],
+    },
   },
   fourthFloor: {
-    numbersDividedBy2: []
+    numbersDividedBy2: [],
   },
   fifthFloor: {
     onlyEven: [],
-    onlyOdd: []
-  }
+    onlyOdd: [],
+  },
 };
 
 for (let counter = 0; counter < data.numbers.length; counter++) {
@@ -72,30 +72,94 @@ console.log(data.fifthFloor.onlyEven);
 
 const data2 = {
   firstFloor: {
-    vowels: []
+    vowels: [],
   },
   secondFloor: {
-    constants: []
+    consonants: [],
   },
 
   fourthFloor: {
-    asciiCode: []
+    asciiCode: [],
   },
   fifthFloor: {
     //Cada palabra de la frase será una posición del array
     wordsInUppercase: [],
-    wordsInLowercase: []
+    wordsInLowercase: [],
   },
   sixthFloor: {
     // En este nivel codificarás la frase para que sea un secreto.
     // Si el caracter es una vocal, la sustituirás por un número siendo a-1 e-2 i-3 o-4 u-5
     // Si el caracter es una consonante deberás sustituirlo por su consonante anterior, si fuera una c, sería una b y si fuera una p sería una ñ y si fuera una v sería una t
     // Si el caracter es un espacio lo sustituirás por una letra random del alfabeto
-    secretCode: ''
-  }
+    secretCode: '',
+  },
 };
-const phrase = newPhrase => {
-  for (let i = 0; i < newPhrase.length; i++) {}
+const saveSentenceInObject = sentence => {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'á', 'é', 'í', 'ó', 'ú'];
+  const vowelsRegexp = /[aeiouáéíóú]/i; //insensitive
+  const lettersRegexp = /[a-zñ]/i;
+  for (const letter of sentence) {
+    // if (vowels.includes(letter.toLowerCase())) {
+    //   data2.firstFloor.vowels.push(letter);
+    // }
+    if (vowelsRegexp.test(letter)) {
+      data2.firstFloor.vowels.push(letter);
+    } else if (lettersRegexp.test(letter)) {
+      data2.secondFloor.consonants.push(letter);
+    }
+
+    data2.fourthFloor.asciiCode.push(letter.charCodeAt());
+  }
+
+  let startPosition = 0;
+  let endPosition = 0;
+  for (let i = 0; i <= sentence.length; i++) {
+    if (sentence[i] === ' ' || i === sentence.length) {
+      endPosition = i;
+      const wordToSave = sentence.substring(startPosition, endPosition);
+      data2.fifthFloor.wordsInLowercase.push(wordToSave.toLowerCase());
+      data2.fifthFloor.wordsInUppercase.push(wordToSave.toUpperCase());
+      startPosition = i + 1;
+    }
+  }
+
+  const consonants = 'bcdfghjklmnñpqrstvwxyz';
+  const alphabet = 'abcdefghijklmnñopqrstuvwxyz';
+
+  let encodedSentence = '';
+
+  for (const letter of sentence.toLowerCase()) {
+    if (letter === 'a' || letter === 'á') {
+      encodedSentence += 1;
+    } else if (letter === 'e' || letter === 'é') {
+      encodedSentence += 2;
+    } else if (letter === 'i' || letter === 'í') {
+      encodedSentence += 3;
+    } else if (letter === 'o' || letter === 'ó') {
+      encodedSentence += 4;
+    } else if (letter === 'u' || letter === 'ú') {
+      encodedSentence += 5;
+    } else if (consonants.includes(letter)) {
+      let consonantPosition;
+      let previousConsonant;
+      if (letter === 'b') {
+        previousConsonant = consonants.length - 1;
+      } else {
+        consonantPosition = consonants.indexOf(letter);
+        previousConsonant = consonantPosition - 1;
+      }
+      encodedSentence += consonants.charAt(previousConsonant);
+    } else if (letter === ' ') {
+      const randomPosition = Math.floor(Math.random() * alphabet.length);
+      const randomLetter = alphabet.charAt(randomPosition);
+      encodedSentence += randomLetter;
+    } else {
+      encodedSentence += letter;
+    }
+
+    data2.sixthFloor.secretCode = encodedSentence;
+  }
+  console.log(data2);
 };
 
-phrase('Si no estudias acabarás como  César');
+saveSentenceInObject('Si no estudias acabarás como Cesár');
